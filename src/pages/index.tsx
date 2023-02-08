33c2d8ab72bcc6ce0f691e421de17490604383a7
Daniel Major <daniel.alves@skytef.com.br>
2023-02-08 12:34:11 -0300

    Tela de home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import camiseta1 from "../assets/1.jpg"
+import camiseta2 from "../assets/2.jpg"
+import camiseta3 from "../assets/3.jpg"
+
 export default function Home() {
   return (
-    <>
-      <h1>teste</h1>
-    </>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
