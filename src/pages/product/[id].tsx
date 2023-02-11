00c7572f8475a8b47e7c173e98f2d4abeca8002b
Daniel Major <daniel.alves@skytef.com.br>
2023-02-11 13:53:19 -0300

    Checkout no stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -15,10 +15,14 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   };
 }
 
 export default function Product({ product }: ProductProps) {
+  function handleTeste() {
+    console.log(product.defaultPriceId)
+  }
   return (
     <ProductContainer>
       <ImageContainer>
@@ -30,7 +34,7 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>Comprar agora</button>
+        <button onClick={handleTeste}>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   );
@@ -65,6 +69,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
           currency: "BRL",
         }).format(price.unit_amount! / 100),
         description: product.description,
+        defaultPriceId: price.id
       },
     },
     revalidate: 60 * 60 * 1,
