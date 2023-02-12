fd199b53593916d6c6cb374099f433b0f12200ab
Daniel Major <daniel.alves@skytef.com.br>
2023-02-12 13:39:32 -0300

    Retornando dados da compra

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,20 +1,57 @@
+import { GetServerSideProps } from "next";
+import Image from "next/image";
 import Link from "next/link";
+import Stripe from "stripe";
+import { stripe } from "../lib/stripe";
 import { ImageContainer, SuccessContainer } from "../styles/pages/success";
 
-export default function Success() {
-    return (
-        <SuccessContainer>
-            <h1>Compra efetuada!</h1>
-
-            <ImageContainer>
-
-            </ImageContainer>
-
-            <p>Uhuul <strong>Daniel Major</strong>, sua <strong>Camiseta teste</strong> Já está a caminho de sua casa</p>
-
-            <Link href="/">
-                Voltar ao cátalogo
-            </Link>
-        </SuccessContainer>
-    )
+interface SuccessProps {
+    customerName: string;
+    product: {
+        name: string;
+        imageUrl: string
+    }
 }
+
+export default function Success({ customerName, product }: SuccessProps) {
+    console.log(product)
+
+    
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+        <Image src={product.imageUrl} alt="" width={120} height={110} />
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>{customerName}</strong>, sua <strong>{product.name}</strong>{" "}
+        Já está a caminho de sua casa
+      </p>
+
+      <Link href="/">Voltar ao cátalogo</Link>
+    </SuccessContainer>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const sessionId = String(query.session_id);
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ["line_items", "line_items.data.price.product"],
+  });
+
+  const customerName = session.customer_details?.name;
+  const product = session.line_items?.data[0].price?.product as Stripe.Product;
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product?.name,
+        imageUrl: product.images[0],
+      },
+    },
+  };
+};
